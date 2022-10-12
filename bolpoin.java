5b6713727b30eb1d8242cdd4b701a9c72fd08322
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2022-10-12 21:29:03 +0700

    Update

--- bolpoin.java
+++ bolpoin.java
@@ -9,11 +9,15 @@ public class bolpoin {
       int bulan = input.nextInt();
       System.out.print("Jumlah Produksi Tiap Bulan : ");
       int produksi = input.nextInt();
-      for (int i = 0; i < bulan;) {
+      int total = 0;
+      for (int i = 0; i < bulan; i++) {
         int j = i + 1;
         System.out.println("Bulan Ke " + j + " : " + jumlah);
+        total = total + jumlah;
         jumlah = jumlah + produksi;
       }
+
+      System.out.println("Total Produksi : " + total);
     } catch (Exception e) {
       System.out.println("Terjadi kesalahan, silahkan coba lagi");
     } finally {
